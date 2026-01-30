0c1639b7e77ed303f94cd07f36b93a91cce988e3
penguinfish <cck20051688@gmail.com>
2026-01-30 18:28:52 -0500

    Remove unwanted commit in run_batch_inference.

--- moshi/moshi/offline.py
+++ moshi/moshi/offline.py
@@ -493,7 +493,7 @@ def run_batch_inference(
                     assert isinstance(hidden_layers, HiddenLayerOutputs), "Hidden layers were requested but not captured."
                     hidden_layers_list.append(hidden_layers)
                     
-                    if i == 0 and c == 0:  # Only log details for first instance
+                    if i == 0 and total_steps == 0:  # Only log details for first instance
                         log("info", f"DEBUG: len(hidden_layers.depth_transformer) = {len(hidden_layers.depth_transformer)}")
                         log("info", f"Retrieved {len(hidden_layers.text_transformer)} text transformer hidden layers and {len(hidden_layers.depth_transformer)} codebooks, each with {len(hidden_layers.depth_transformer[0])} layers")
                 else:
